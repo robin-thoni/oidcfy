0f141b12062a59bc0d5545e7b424542add84d847
Robin THONI <robin@rthoni.com>
2023-05-27 16:49:16 +0200

    added template utils to render int and bool

--- internal/utils/template.go
+++ internal/utils/template.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bytes"
+	"strconv"
 	"text/template"
 )
 
@@ -13,4 +14,30 @@ func RenderTemplate(tpl *template.Template, ctx interface{}) (string, error) {
 	}
 
 	return buf.String(), nil
+}
+
+func RenderTemplateInt(tpl *template.Template, ctx interface{}) (int, error) {
+	valueStr, err := RenderTemplate(tpl, ctx)
+	if err != nil {
+		return 0, err
+	}
+	value, err := strconv.ParseInt(valueStr, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(value), nil
+}
+
+func RenderTemplateBool(tpl *template.Template, ctx interface{}) (bool, error) {
+	valueStr, err := RenderTemplate(tpl, ctx)
+	if err != nil {
+		return false, err
+	}
+	value, err := strconv.ParseBool(valueStr)
+	if err != nil {
+		return false, err
+	}
+
+	return value, nil
 }
